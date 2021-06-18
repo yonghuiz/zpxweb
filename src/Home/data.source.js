b5eb553974ca50b72f5dfa8ce1d519e77d0d4cfa
yonghuiz <yonghuiz@outlook.com>
2021-06-18 18:48:10 -0500

    more

--- src/Home/data.source.js
+++ src/Home/data.source.js
@@ -397,7 +397,9 @@ export const Content00DataSource = {
               className: 'content0-block-title',
               children: 'Apartment/Condo',
             },
-            { name: 'content', children: 'As a property manager, do you feel your precious time is wasted everyday to sign and manage packages? As a resident, have you ever lost your packages or unable to retrieve them after office hours? Zippora is here to solve all the problems!' },
+            { name: 'content',
+			className: 'content0-block-content',
+			 children: 'As a property manager, do you feel your precious time is wasted everyday to sign and manage packages? As a resident, have you ever lost your packages or unable to retrieve them after office hours? Zippora is here to solve all the problems!' },
           ],
         },
       },
@@ -422,6 +424,7 @@ export const Content00DataSource = {
             },
             {
               name: 'content',
+			  className: 'content0-block-content',
               children: 'As a property manager, do you feel the property is not secured when carriers visit offices to deliver packages? As an employee, are you concerned about your privacy when colleagues sign packages for you? Zippora is here to solve all the problems!',
             },
           ],
@@ -448,6 +451,7 @@ export const Content00DataSource = {
             },
             {
               name: 'content',
+			  className: 'content0-block-content',
               children: 'As a school faculty, do you feel tedious to handle tons of packages every day for all the teachers and students? As a student, do you often have food delivered to campus yet you are right in the middle of a class? Zippora is here to solve all the problems!',
             },
           ],
@@ -474,6 +478,7 @@ export const Content00DataSource = {
             },
             {
               name: 'content',
+			  className: 'content0-block-content',
               children: 'As a store manager, don’t you think you are spending too much time on helping customers to pickup their groceries? As a grocery shopper, do you feel you have wasted too much time on pushing a shopping cart around and waiting to pay? Zippora is here to solve all the problems!',
             },
           ],
